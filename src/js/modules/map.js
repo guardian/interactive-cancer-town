6817b0e49eb71d41365bd4623746165e5716dd72
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-04-10 17:42:53 -0400

    promises are working

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,10 +1,21 @@
-import L from 'leaflet';
+import * as d3 from 'd3';
 
-let map;
+let width, height, canvas, ctx, projection;
 
-const bounds = {
-    'america': L.latLngBounds(L.latLng(49.112152, -127.143063), L.latLng(20.714286, -70.209896))
-}
+let layers = [
+    {
+        url: '{{ path }}/assets/america.png',
+        coords: [[49.112152, -127.143063],[20.714286, -70.209896]]
+    },
+    {
+        url: '{{ path }}/assets/louisiana.png',
+        coords: [[33.505597, -94.819785],[28.514333, -88.349533]]
+    },
+    {
+        url: '{{ path }}/assets/louisiana.png',
+        coords: [[30.608970, -91.247849], [29.430020, -89.294753]]
+    }
+]
 
 export default {
     init: function() {
@@ -12,27 +23,54 @@ export default {
     },
 
     createMap: function() {
-        map = L.map('uit-visual__map', {
-            center: bounds.america.getCenter(),
-            maxBounds: bounds.america,
-            maxBoundsViscosity: 1.0,
-            zoom: 2,
-            zoomSnap: 0,
-            renderer: L.canvas()
+        width = $('.uit-visual__map').width();
+        height = $('.uit-visual__map').height();
+
+        projection = d3.geoAlbersUsa()
+            .scale(1)
+            .translate([width / 2, height / 2]);
+
+        const path = d3.geoPath()
+            .projection(projection);
+
+        const zoom = d3.zoom()
+            .scaleExtent([1, 8]);
+
+        canvas = d3.select('.uit-visual__map')
+            .append('canvas')
+            .attr('width', width)
+            .attr('height', height);
+
+        ctx = canvas.node().getContext('2d');
+
+        var images = layers.map(layer => this.loadImage(layer.url));
+
+        Promise.all(images)
+            .then(images => {
+                images.forEach((image, i) => {
+                    layers[i].image = image;
+                })
+                this.draw();
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    },
+
+    loadImage: function(url) {
+        return new Promise((fulfill, reject) => {
+            let image = new Image();
+            image.onload = () => fulfill(image);
+            image.src = url;
+        });
+    },
+
+    draw: function() {
+        ctx.clearRect(0, 0, width, height);
+
+        layers.forEach(layer => {
+            ctx.drawImage(layer.image, projection(layer.coords[0]), projection(layer.coords[1]));
         });
 
-        map.fitBounds(bounds.america);
-
-        // map.dragging.disable();
-        // map.touchZoom.disable();
-        // map.doubleClickZoom.disable();
-        // map.scrollWheelZoom.disable();
-        // map.boxZoom.disable();
-        // map.keyboard.disable();
-
-        console.log('{{ path }}/assets/america.png');
-
-        let america = L.imageOverlay('{{ path }}/assets/america.png', bounds.america);
-            america.addTo(map);
     }
 }
