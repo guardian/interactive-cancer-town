365897fccec587624f347c645715c9932c9debe5
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-04-18 14:42:31 -0400

    add state outlines to 2nd phase of map

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,24 +1,6 @@
 import * as d3 from 'd3';
 
-let width, height, svg, ctx, projection, zoom, scale, translate, annotations;
-
-let labels = [
-    {
-        text: 'Louisiana 51',
-        coords: [-92.081339, 31.177374],
-        layer: 0
-    },
-    {
-        text: 'Reserve',
-        coords: [-90.554791, 30.061910],
-        layer: 2
-    },
-    {
-        text: 'New Orleans',
-        coords: [-90.0715, 29.9511],
-        layer: 1
-    }
-]
+let width, height, svg, ctx, projection, zoom, scale, translate, placeNames;
 
 let layers = [
     {
@@ -28,15 +10,49 @@ let layers = [
     },
     {
         url: '{{ path }}/assets/louisiana.png',
-        coords: [[-94.03945383090473, 33.087156474194146],[-89.01863607235484, 28.985983009532934]],
+        coords: [[-94.13945383090473, 33.287156474194146],[-88.91863607235484, 28.785983009532934]],
         fillScale: .8
     },
     {
         url: '{{ path }}/assets/reserve.svg',
         coords: [[-90.7498, 30.2383], [-90.2012, 29.8341]],
-        fillScale: 1
+        fillScale: 1.2
     }
 ];
+
+let states = [
+    {
+        text: 'Louisiana',
+        coords: [-92.081339, 31.177374],
+        layer: 0
+    }
+]
+
+let places = [
+    {
+        text: 'Reserve',
+        coords: [-90.554791, 30.061910],
+        layer: 2
+    },
+    {
+        text: 'New Orleans',
+        coords: [-90.0715, 29.9511],
+        layer: 1
+    },
+    {
+        text: 'Baton Rouge',
+        coords: [-91.1871, 30.4515],
+        layer: 1
+    }
+]
+
+let images = [
+    {
+        url: '{{ path }}/assets/state-outlines.svg',
+        coords: [[-127.867851146563, 47.89786873427608],[-74.26175764877926, 23.16732698168454]],
+        layer: 1
+    }
+]
 
 export default {
     init: function() {
@@ -55,30 +71,60 @@ export default {
         zoom = d3.zoom().scaleExtent([1, 1000]).on('zoom', function() {
             var transform = d3.event.transform;
             g.attr('transform', transform);
-            annotations.attr('style', 'font-size: ' + (16 / transform.k) + 'px');
+            placeLabels.attr('style', 'font-size: ' + (16 / transform.k) + 'px');
+            placeLabels.select('circle').attr('r', 4 / transform.k);
+            placeLabels.select('text').attr('y', -10 / transform.k);
         }.bind(this));
 
         var g = svg.append('g');
 
-        var images = g.selectAll('image')
-            .data(layers)
+        g.selectAll('uit-visuals__map-image--additional')
+            .data(images)
             .enter()
             .append('svg:image')
             .attr('xlink:href', function(d) { return d.url })
-            .attr('class', function(d, i) { return 'uit-visuals__map-image uit-visuals__map-image--' + i})
+            .attr('class', function(d, i) { return 'uit-visuals__map-image uit-visuals__map-image--additional uit-visuals__map-image--' + d.layer})
             .attr('x', function(d) { return projection(d.coords[0])[0] })
             .attr('y', function(d) { return projection(d.coords[0])[1] })
             .attr('width', function(d) { return projection(d.coords[1])[0] - projection(d.coords[0])[0] })
             .attr('height', function(d) { return projection(d.coords[1])[1] - projection(d.coords[0])[1] });
 
-        annotations = g.selectAll('text')
-            .data(labels)
+        g.selectAll('.uit-visuals__map-image--base')
+            .data(layers)
             .enter()
-            .append('svg:text')
+            .append('svg:image')
+            .attr('xlink:href', function(d) { return d.url })
+            .attr('class', function(d, i) { return 'uit-visuals__map-image uit-visuals__map-image--base uit-visuals__map-image--' + i})
+            .attr('x', function(d) { return projection(d.coords[0])[0] })
+            .attr('y', function(d) { return projection(d.coords[0])[1] })
+            .attr('width', function(d) { return projection(d.coords[1])[0] - projection(d.coords[0])[0] })
+            .attr('height', function(d) { return projection(d.coords[1])[1] - projection(d.coords[0])[1] });
+
+        var placeLabels = g.selectAll('.uit-visual__map-label')
+            .data(places)
+            .enter()
+            .append('g')
             .attr('class', function(d) { return 'uit-visual__map-label uit-visual__map-label--' + d.layer })
-            .attr('x', function(d) { return projection(d.coords)[0] })
-            .attr('y', function(d) { return projection(d.coords)[1] })
+            .attr('transform', function(d) { return `translate(${projection(d.coords)[0]}, ${projection(d.coords)[1]})` })
+
+        placeLabels.append('circle')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('r', 4);
+
+        placeLabels.append('svg:text')
+            .attr('x', 0)
+            .attr('y', 0)
             .text(function(d) { return d.text });
+
+        var stateLabels = g.selectAll('.uit-visual__map-state')
+            .data(states)
+            .enter()
+            .append('text')
+            .attr('class', function(d) { return 'uit-visual__map-state uit-visual__map-state--' + d.layer })
+            .attr('x', function(d) { return projection(d.coords)[0] })
+            .attr('y', function(d) { return projection(d.coords)[0] })
+            .text(function(d) { return d.text })
 
         this.zoomTo(layers[0], true);
     },
